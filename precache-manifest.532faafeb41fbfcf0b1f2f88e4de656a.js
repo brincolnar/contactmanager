06a5fb0f61b978998b8905ff8028f336f53cd220
brincolnar <brincolnar2000@gmail.com>
2020-08-26 15:56:07 +0200

    Updates

---
+++ precache-manifest.532faafeb41fbfcf0b1f2f88e4de656a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7564cc6d769a5b0b05a235dd7724e943",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3580a9b8c6c2a1453032",
+    "url": "/contactmanager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9c64adb4b7b61062067b",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3580a9b8c6c2a1453032",
+    "url": "/contactmanager/static/js/2.2cfb2e15.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.2cfb2e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c64adb4b7b61062067b",
+    "url": "/contactmanager/static/js/main.cd39de1f.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
